a64b457cec30b0bcf27086c6421b0011321b1bb5
Dave Turner <davetii@gmail.com>
2021-05-28 16:50:53 -0400

    useState react hook example

--- src/components/expenses/ExpenseItem.js
+++ src/components/expenses/ExpenseItem.js
@@ -1,19 +1,24 @@
+import React, {useState} from "react";
+
 import './ExpenseItem.css';
 import ExpenseDate from "./ExpenseDate";
 import Card from "../ui/Card";
 
 const ExpenseItem = (props) => {
+    // a react hook
+    const [title, setTitle] = useState(props.title);
+
     const onChangeTitle = () => {
-        console.log('hey you')
+        setTitle("bob");
     };
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date}/>
             <div className="expense-item__description">
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className="expense-item__price">{props.amount}</div>
             </div>
-            <button onClick={onChangeTitle}>chaneg title</button>
+            <button onClick={onChangeTitle}>change title</button>
         </Card>
     );
 }
