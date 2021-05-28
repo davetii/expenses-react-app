6a69dfe690ec2340f4544a92084a0ae4b016867c
Dave Turner <davetii@gmail.com>
2021-05-28 18:33:34 -0400

    Removed unncesary button in expense item

--- src/components/expenses/ExpenseItem.js
+++ src/components/expenses/ExpenseItem.js
@@ -5,20 +5,13 @@ import ExpenseDate from "./ExpenseDate";
 import Card from "../ui/Card";
 
 const ExpenseItem = (props) => {
-    // a react hook
-    const [title, setTitle] = useState(props.title);
-
-    const onChangeTitle = () => {
-        setTitle("bob");
-    };
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date}/>
             <div className="expense-item__description">
-                <h2>{title}</h2>
+                <h2>{props.title}</h2>
                 <div className="expense-item__price">{props.amount}</div>
             </div>
-            <button onClick={onChangeTitle}>change title</button>
         </Card>
     );
 }
