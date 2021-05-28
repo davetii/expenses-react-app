3e2fe278e38a883c75ec04166b6bc1b3c76db0f7
Dave Turner <davetii@gmail.com>
2021-05-28 18:13:00 -0400

    Improving ExpenseFilter code

--- src/components/expenses/Expenses.js
+++ src/components/expenses/Expenses.js
@@ -1,18 +1,21 @@
+import React, {useState} from "react";
 import ExpenseItem from "./ExpenseItem";
 import './Expenses.css';
 import Card from "../ui/Card";
 import ExpenseFilter from "./ExpensesFilter";
 
 const Expenses = (props) => {
+    const [filteredYear, setFilteredYear] = useState('2020');
     const expenses = props.expenses;
-    const yearChanged = (data) => {
+    const yearChanged = (selectedYear) => {
+        setFilteredYear(selectedYear);
         console.log("yearChanged");
-        console.log(data);
+        console.log(selectedYear);
     }
     return(
         <div>
             <Card className="expenses">
-                <ExpenseFilter onChangeYear={yearChanged}/>
+                <ExpenseFilter selected={filteredYear} onChangeYear={yearChanged}/>
                 <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}></ExpenseItem>
                 <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}></ExpenseItem>
                 <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}></ExpenseItem>
