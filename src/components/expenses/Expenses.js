e5c220bf58a4ea44242219a96ea348996c141891
Dave Turner <davetii@gmail.com>
2021-06-26 13:53:56 -0400

    Implemented filter logic

--- src/components/expenses/Expenses.js
+++ src/components/expenses/Expenses.js
@@ -6,18 +6,16 @@ import ExpenseFilter from "./ExpensesFilter";
 
 const Expenses = (props) => {
     const [filteredYear, setFilteredYear] = useState('2020');
-    const expenses = props.expenses;
-    const yearChanged = (selectedYear) => {
-        setFilteredYear(selectedYear);
-        console.log("yearChanged");
-        console.log(selectedYear);
-    }
+    const yearChanged = (selectedYear) => { setFilteredYear(selectedYear);}
+    const filteredExpense = props.expenses.filter(i => {
+        return i.date.getFullYear() === +filteredYear;
+    })
     return(
         <div>
             <Card className="expenses">
                 <ExpenseFilter selected={filteredYear} onChangeYear={yearChanged}/>
                 {
-                    props.expenses.map((expense) => (
+                    filteredExpense.map((expense) => (
                         <ExpenseItem
                             key={expense.id}
                             title={expense.title}
